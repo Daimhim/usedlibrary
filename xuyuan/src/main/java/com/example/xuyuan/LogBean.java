2136a3c751c6ab062e2f8caf15e5a1d1ad09f168
Daimhim <daimhim@163.com>
2017-06-03 17:19:12 +0800

    one

---
+++ xuyuan/src/main/java/com/example/xuyuan/LogBean.java
@@ -0,0 +1,15 @@
+package com.example.xuyuan;
+
+/**
+ * 项目名称：com.example.xuyuan
+ * 项目版本：usedlibrary
+ * 创建人：Daimhim
+ * 创建时间：2017/6/3 17:18
+ * 修改人：Daimhim
+ * 修改时间：2017/6/3 17:18
+ * 类描述：
+ * 修改备注：
+ */
+
+public class LogBean {
+}

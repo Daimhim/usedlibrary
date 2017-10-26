c2063f1b72b81998647197fb0a3d9d95a1f371d2
Daimhim <daimhim@163.com>
2017-10-26 13:53:23 +0800

    init

---
+++ used/src/main/java/com/example/used/decoration/DecorationBuilder2.java
@@ -0,0 +1,16 @@
+package com.example.used.decoration;
+
+/**
+ * 项目名称：com.example.used.decoration
+ * 项目版本：usedlibrary
+ * 创建人：Daimhim
+ * 创建时间：2017/10/26 11:23
+ * 修改人：Daimhim
+ * 修改时间：2017/10/26 11:23
+ * 类描述：
+ * 修改备注：
+ */
+
+public class DecorationBuilder2 {
+
+}
--- used/src/main/java/com/example/used/decoration/RecycleDecoration2.java
+++ used/src/main/java/com/example/used/decoration/RecycleDecoration2.java
@@ -19,16 +19,56 @@ import android.widget.ListView;
  */
 
 public class RecycleDecoration2 extends RecyclerView.ItemDecoration{
-
+    protected DrawBeforeTarget mDrawBeforeTarget;
+    protected DrawAfterTarget mDrawAfterTarget;
+    protected MeasureTarget mMeasureTarget;
     @Override
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        if (null!=mDrawBeforeTarget){
+            mDrawBeforeTarget.onDraw(c, parent, state);
+        }
     }
 
     @Override
     public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        if (null!=mDrawAfterTarget){
+            mDrawAfterTarget.onDrawOver(c, parent, state);
+        }
     }
 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        if (null!=mMeasureTarget){
+            mMeasureTarget.getItemOffsets(outRect, view, parent, state);
+        }
+    }
+
+    interface DrawBeforeTarget{
+        /**
+         * 目标绘制之前
+         * @param c 画板
+         * @param parent  Recycler
+         * @param state   状态
+         */
+        void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state);
+    }
+    interface DrawAfterTarget{
+        /**
+         * 目标绘制之后
+         * @param c 画板
+         * @param parent  Recycler
+         * @param state   状态
+         */
+        void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state);
+    }
+    interface MeasureTarget{
+        /**
+         * 测量
+         * @param outRect 结果
+         * @param view   目标item
+         * @param parent RecyclerView
+         * @param state  状态  可保存数据
+         */
+        void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state);
     }
 }
